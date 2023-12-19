2858643df4938bf8bad39ff3be56f0d96597c89e
Dennis Wilson <d9w@pm.me>
2023-12-19 07:51:07 +0100

    2023 updates to RNN class

--- RNN/solutions/1_future.py
+++ RNN/solutions/1_future.py
@@ -1,23 +1,23 @@
 class RNN(nn.Module):
     def __init__(self, input_size, hidden_size, output_size):
         super(RNN, self).__init__()
+        self.input_size = input_size
         self.hidden_size = hidden_size
-        self.rnn1 = nn.RNNCell(input_size, hidden_size)
-        self.linear = nn.Linear(hidden_size, output_size)
+        self.rnn1 = nn.RNNCell(1, hidden_size)
+        self.linear = nn.Linear(hidden_size, 1)
 
-    def forward(self, input, hidden, future=0):
-        # predict over the different signals
+    def forward(self, inp, hidden, future=0):
         outputs = []
-        for input_t in input.split(1, dim=1):
-            hidden = self.rnn1(input_t, hidden)
+        for signal in inp.split(1, dim=1):
+            hidden = self.rnn1(signal, hidden)
             output = self.linear(hidden)
             outputs += [output]
-        for i in range(future):# if we should predict the future
+        for i in range(1, future):# if we should predict the future
             hidden = self.rnn1(output, hidden)
             output = self.linear(hidden)
             outputs += [output]
         outputs = torch.cat(outputs, dim=1)
         return outputs, hidden
-    
+
     def init_hidden(self):
-        return torch.zeros(input.size(0), self.hidden_size, dtype=torch.double)
+        return torch.zeros(self.input_size, self.hidden_size, dtype=torch.double)
--- RNN/solutions/3_lstm.py
+++ RNN/solutions/3_lstm.py
@@ -1,25 +1,25 @@
 class LSTM(nn.Module):
     def __init__(self, input_size, hidden_size, output_size):
         super(LSTM, self).__init__()
-        self.lstm1 = nn.LSTMCell(input_size, hidden_size)
-        self.linear = nn.Linear(hidden_size, output_size)
         self.input_size = input_size
         self.hidden_size = hidden_size
-        self.output_size = output_size
+        self.lstm1 = nn.LSTMCell(1, hidden_size)
+        self.linear = nn.Linear(hidden_size, 1)
 
-    def forward(self, input, h_t, c_t, future=0):
+    def forward(self, inp, h_t, c_t, future=0):
         outputs = []
-        for input_t in input.split(1, dim=1):
-            h_t, c_t = self.lstm1(input_t, (h_t, c_t))
+        for signal in inp.split(1, dim=1):
+            h_t, c_t = self.lstm1(signal, (h_t, c_t))
             output = self.linear(h_t)
             outputs += [output]
-        for i in range(future):# if we should predict the future
+        for i in range(1, future):# if we should predict the future
             h_t, c_t = self.lstm1(output, (h_t, c_t))
             output = self.linear(h_t)
             outputs += [output]
         outputs = torch.cat(outputs, dim=1)
-        return outputs
-    
-    def init_states(self):
-        #initialize hidden and cell states
-        return (torch.zeros(input.size(0), self.hidden_size, dtype=torch.double),torch.zeros(input.size(0), self.hidden_size, dtype=torch.double))
+        return outputs, h_t, c_t
+
+    def init_hidden(self):
+        return (torch.zeros(self.input_size, self.hidden_size, dtype=torch.double),
+                torch.zeros(self.input_size, self.hidden_size, dtype=torch.double))
+
